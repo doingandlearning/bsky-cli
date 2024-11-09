d7d7d38ee0b8d78974ce4f35acb792a250706a98
Kevin Cunningham <github@kevincunningham.co.uk>
2024-11-09 11:35:05 +0000

    excludes repeated posts from the stream

--- cmd/cli/stream.go
+++ cmd/cli/stream.go
@@ -7,6 +7,7 @@ import (
 
 func streamLatestPosts(authToken, did string, interval time.Duration) {
 	var lastSeenURI string
+	displayedReposts := make(map[string]bool) // Track reposts that have been displayed
 
 	for {
 		// Fetch the latest posts
@@ -19,7 +20,11 @@ func streamLatestPosts(authToken, did string, interval time.Duration) {
 
 		// Iterate over posts and print only new ones
 		for _, post := range posts {
-			postData := post.(map[string]interface{})
+			postData, ok := post.(map[string]interface{})
+			if !ok {
+				continue
+			}
+
 			uri, _ := postData["post"].(map[string]interface{})["uri"].(string)
 
 			// Stop printing if we reach the last seen post
@@ -27,11 +32,21 @@ func streamLatestPosts(authToken, did string, interval time.Duration) {
 				break
 			}
 
+			// Check if this is a repost by looking for the "reason" field
+			if _, isRepost := postData["reason"]; isRepost {
+				// Skip if this repost has already been displayed
+				if displayedReposts[uri] {
+					continue
+				}
+				// Mark this repost URI as displayed
+				displayedReposts[uri] = true
+			}
+
 			// Display the post
 			PrintPost(postData)
 		}
 
-		// Update the last seen URI
+		// Update the last seen URI to the latest post's URI
 		if len(posts) > 0 {
 			firstPost := posts[0].(map[string]interface{})
 			lastSeenURI, _ = firstPost["post"].(map[string]interface{})["uri"].(string)
